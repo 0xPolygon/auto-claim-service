5c9bd5586dcf559131470c87a5688996947d4f6a
Nitin Mittal <nitinmittal44066@gmail.com>
2025-10-08 15:12:52 +0530

    feat: new common config for autoclaim

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Logger } from "@polygonlabs/servercore";
 import AutoClaimService from "./services/auto-claim.js";
-import { ethers } from 'ethers';
-import config from "./config/index.js";
+import { ethers, FetchRequest } from 'ethers';
 import bridgeAbi from "./abi/bridge.js";
 import SlackNotify from "./services/slack-notify.js";
 import GasStation from "./services/gas-station.js";
@@ -9,7 +8,7 @@ import TransactionService from "./services/transaction.js";
 
 Logger.create({
     sentry: {
-        dsn: config.LOGGER.SENTRY_DSN,
+        dsn: process.env.SENTRY_DSN,
         level: 'error'
     },
     console: {
@@ -18,44 +17,66 @@ Logger.create({
 });
 
 let autoClaimService: AutoClaimService;
+
+const POLL_INTERVAL = 30000; // 30 seconds
 async function run() {
     while (true) {
-        await autoClaimService.claimTransactions();
-        await new Promise(r => setTimeout(r, 30000));
+        try {
+            await autoClaimService.claimTransactions();
+        } catch (error) {
+            Logger.error({ error, message: "Error claiming transactions" });
+        }
+        await new Promise(r => setTimeout(r, POLL_INTERVAL));
     }
+}
+
+function createFetchRequest(chainId: string): FetchRequest {
+    const rawRPCConfig = JSON.parse(process.env.RPC_CONFIG || "{}");
+    const url = rawRPCConfig[chainId] || `${process.env.BASE_ERPC_URL}/${chainId}`;
+    const fetchRequest = new FetchRequest(url);
+    if (process.env.ERPC_API_KEY) {
+        fetchRequest.setHeader("X-ERPC-Secret-Token", process.env.ERPC_API_KEY);
+    }
+    return fetchRequest;
+}
+
+function createEthersClients(): { [key: string]: ethers.JsonRpcProvider } {
+    let ethersClients: { [key: string]: ethers.JsonRpcProvider } = {}
+    const sourceNetworks = JSON.parse(process.env.SOURCE_NETWORKS || "[]");
+    const sourceNetworkChainIds = JSON.parse(process.env.SOURCE_NETWORK_CHAINIDS || "[]");
+    for (let index = 0; index < sourceNetworks.length; index += 1) {
+        const fetchRequest = createFetchRequest(sourceNetworkChainIds[index]);
+        ethersClients[sourceNetworks[index]] = new ethers.JsonRpcProvider(fetchRequest)
+    }
+
+    return ethersClients;
 }
 
 async function start() {
     try {
+        const provider = new ethers.JsonRpcProvider(createFetchRequest(process.env.DESTINATION_NETWORK_CHAINID as string));
+        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
+        const ethersClients = createEthersClients();
 
-        const provider = new ethers.JsonRpcProvider(config.RPC_URL);
-        const wallet = new ethers.Wallet(config.PRIVATE_KEY as string, provider);
+        const bridgeContract = new ethers.Contract(
+            process.env.BRIDGE_CONTRACT as string,
+            bridgeAbi,
+            wallet
+        );
 
-        let slackNotify = null;
-        if (config.SLACK_URL) {
-            slackNotify = new SlackNotify(config.SLACK_URL)
-        }
-
-        let ethersClients: { [key: string]: ethers.JsonRpcProvider } = {}
-        for (let index = 0; index < JSON.parse(config.SOURCE_NETWORKS).length; index += 1) {
-            ethersClients[JSON.parse(config.SOURCE_NETWORKS)[index]] = new ethers.JsonRpcProvider(JSON.parse(config.SOURCE_NETWORKS_RPC)[index])
-        }
+        const transactionService = new TransactionService(
+            process.env.BRIDGE_HUB_API_URL as string,
+            process.env.SOURCE_NETWORKS as string,
+            process.env.DESTINATION_NETWORK as string,
+            ethersClients
+        );
 
         autoClaimService = new AutoClaimService(
-            new ethers.Contract(
-                config.BRIDGE_CONTRACT as string,
-                bridgeAbi,
-                wallet
-            ),
-            new TransactionService(
-                config.BRIDGE_HUB_API_URL as string,
-                config.SOURCE_NETWORKS,
-                config.DESTINATION_NETWORK as string,
-                ethersClients,
-            ),
-            new GasStation(config.GAS_STATION_URL as string),
-            config.DESTINATION_NETWORK as string,
-            slackNotify
+            bridgeContract,
+            transactionService,
+            new GasStation(process.env.GAS_STATION_URL as string),
+            process.env.DESTINATION_NETWORK as string,
+            process.env.SLACK_URL ? new SlackNotify(process.env.SLACK_URL) : null
         );
 
         run();
